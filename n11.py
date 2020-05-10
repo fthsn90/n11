4fcd4a49df998a0c20a6fba5433a34d7c9a986ae
Fatih Shen <64291419+fthsn90@users.noreply.github.com>
2020-05-10 21:38:19 +0300

    Add files via upload

--- n11.py
+++ n11.py
@@ -3,20 +3,24 @@ from bs4 import BeautifulSoup
 import re
 import string
 
+class n11():
+    def __init__(self,kate):
+        self.url = "https://www.n11.com/"+str(kate)
+        self.response = requests.get(self.url)
+        self.html = self.response.content
+        self.soup = BeautifulSoup(self.html, "lxml")
+
+    def vericek(self):
+        self.liler = self.soup.findAll("li",attrs={"class":"column"})
+        for i in self.liler:
+            self.urunadi = i.find("div",attrs={"class":"pro"}).text
+            self.fiyat = i.find("ins").text
+            self.fiyat1 = self.fiyat.translate({ord(c): None for c in string.whitespace}) # BOŞLUK FELAN BIRAKMA LA
+            print("***********************************************")
+            print("Ürün Adı: {}\n".format(re.sub(r"\s+", " ", self.urunadi), sep=' ')) # Boşluk
+            print("Fiyatı  : {}".format(self.fiyat1))
 
 
-url = "https://www.n11.com/bilgisayar"
-
-response = requests.get(url)
-html = response.content
-soup = BeautifulSoup(html, "lxml")
-
-liler = soup.findAll("li",attrs={"class":"column"}) 
-
-for i in liler:
-    urunadi = i.find("div",attrs={"class":"pro"}).text
-    fiyat = i.find("ins").text
-    fiyat1 = fiyat.translate({ord(c): None for c in string.whitespace}) # BOŞLUK FELAN BIRAKMA LA
-    print("***********************************************")
-    print("Ürün Adı: {}\n".format(re.sub(r"\s+", " ", urunadi), sep=' ')) # Boşluk
-    print("Fiyatı  : {}".format(fiyat1))
+ne = input("Çekmek istediğiniz kategoriyi giriniz:")
+vss = n11(ne)
+vss.vericek()
